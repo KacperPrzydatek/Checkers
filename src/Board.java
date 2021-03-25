8aeca7007bb374f5a42514bcb485afea182dab25
kprzydat <kprzydatek4@gmail.com>
2021-03-26 00:18:39 +0100

    ProjectStart

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,20 @@
 public class Board {
-    Square[][] board=new Square[8][8];
+    private
+    Square[][] _board=new Square[8][8];
+    public
+    void startGame()
+    {
+        for(int i=0;i<8;i++)
+        {
+            for(int j=0;j<8;j++)
+            {
+                if((i+j)%2==0)
+                {
+                    _board[i][j].moveIn(Global.Empty);
+                }
+
+            }
+        }
+    }
+
 }
--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,44 @@
 public class Square {
+    private
+            int _nameVertical;
+            int _nameHorizontal;
+            boolean _ifBlack;
+            Piece _content; // mark for piece placed on this square
+    public
+
+    Square(int nameVer,int nameHor, boolean colour)
+    {
+        _nameVertical=nameVer;
+        _nameHorizontal=nameHor;
+        _ifBlack=colour;
+    }
+    void moveIn(Piece piece)
+    {
+        if(_content!=Global.Empty)
+        {
+            System.out.println("There is another piece on this square. Do another move.");
+            System.out.println("MMark1 wstaw modal wyswietlajacy sie uzytkownikowi");
+        }
+        else
+        {
+            _content=piece;
+        }
+    }
+    void moveOut()
+    {
+        if(_content==Global.Empty)
+        {
+            System.out.println("There is no piece on this square. Do another move.");
+            System.out.println("MMark2 wstaw modal wyswietlajacy sie uzytkownikowi");
+        }
+        else
+        {
+            _content=Global.Empty;
+        }
+    }
+    boolean amIEmpty()
+    {
+        if(_content!=Global.Empty) return false;
+        else return true;
+    }
 }
